d2d4c1a5c2e7befdbfcdc0b6f6762f0bc257674a
Ghitado <thiaguinho.020405@gmail.com>
2025-11-19 15:28:19 -0300

    test(domain/tests/entities): add PersonTests covering constructor and UpdateName validation

---
+++ tests/VaccinationManager.Domain.Tests/Entities/PersonTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using VaccinationManager.Domain.Common;
+using VaccinationManager.Domain.Entities;
+
+namespace VaccinationManager.Domain.Tests.Entities;
+
+public class PersonTests
+{
+    [Fact]
+    public void Constructor_WithValidName_CreatesPerson()
+    {
+        var person = new Person("John");
+
+        person.Name.Should().Be("John");
+        person.Id.Should().NotBeEmpty();
+    }
+
+    [Fact]
+    public void Constructor_WithEmptyName_ThrowsException()
+    {
+        Action act = () => new Person("");
+
+        act.Should().Throw<DomainException>()
+            .WithMessage("Name is required.");
+    }
+
+    [Fact]
+    public void UpdateName_WithValidName_Updates()
+    {
+        var person = new Person("Old");
+
+        person.UpdateName("New");
+
+        person.Name.Should().Be("New");
+    }
+
+    [Fact]
+    public void UpdateName_WithEmptyName_ThrowsException()
+    {
+        var person = new Person("Valid");
+
+        Action act = () => person.UpdateName("");
+
+        act.Should().Throw<DomainException>()
+            .WithMessage("Name is required.");
+    }
+}
