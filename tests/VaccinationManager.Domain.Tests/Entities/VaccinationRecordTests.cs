9f9cc6d41103c629a36547c067327e21f2f1bf19
Ghitado <thiaguinho.020405@gmail.com>
2025-11-19 15:32:19 -0300

    test(domain/tests/entities): add VaccinationRecordTests covering constructor, Date and Dose validation

---
+++ tests/VaccinationManager.Domain.Tests/Entities/VaccinationRecordTests.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using VaccinationManager.Domain.Common;
+using VaccinationManager.Domain.Entities;
+
+namespace VaccinationManager.Domain.Tests.Entities;
+
+public class VaccinationRecordTests
+{
+	[Fact]
+	public void Constructor_WithValidData_CreatesRecord()
+	{
+		var personId = Guid.NewGuid();
+		var vaccineId = Guid.NewGuid();
+
+		var record = new VaccinationRecord(personId, vaccineId, DateTime.Today, 1);
+
+		record.PersonId.Should().Be(personId);
+		record.VaccineId.Should().Be(vaccineId);
+		record.Dose.Should().Be(1);
+	}
+
+	[Fact]
+	public void Constructor_WithEmptyPersonId_Throws()
+	{
+		Action act = () => new VaccinationRecord(Guid.Empty, Guid.NewGuid(), DateTime.Today, 1);
+
+		act.Should().Throw<DomainException>()
+			.WithMessage("PersonId is required.");
+	}
+
+	[Fact]
+	public void Constructor_WithEmptyVaccineId_Throws()
+	{
+		Action act = () => new VaccinationRecord(Guid.NewGuid(), Guid.Empty, DateTime.Today, 1);
+
+		act.Should().Throw<DomainException>()
+			.WithMessage("VaccineId is required.");
+	}
+
+	[Fact]
+	public void Constructor_WithInvalidDate_Throws()
+	{
+		Action act = () => new VaccinationRecord(Guid.NewGuid(), Guid.NewGuid(), default, 1);
+
+		act.Should().Throw<DomainException>()
+			.WithMessage("Invalid date.");
+	}
+
+	[Fact]
+	public void Constructor_WithInvalidDose_Throws()
+	{
+		Action act = () => new VaccinationRecord(Guid.NewGuid(), Guid.NewGuid(), DateTime.Today, 0);
+
+		act.Should().Throw<DomainException>()
+			.WithMessage("Invalid dose.");
+	}
+}
