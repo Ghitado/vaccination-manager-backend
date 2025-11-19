47cc03fe419439a4d5de60a5782a8dbc033a07fd
Ghitado <thiaguinho.020405@gmail.com>
2025-11-19 15:32:00 -0300

    test(domain/tests/entities): add VaccineTests covering constructor and UpdateName validation

---
+++ tests/VaccinationManager.Domain.Tests/Entities/VaccineTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using VaccinationManager.Domain.Common;
+using VaccinationManager.Domain.Entities;
+
+namespace VaccinationManager.Domain.Tests.Entities;
+
+public class VaccineTests
+{
+	[Fact]
+	public void Constructor_WithValidName_CreatesVaccine()
+	{
+		var vaccine = new Vaccine("COVID");
+
+		vaccine.Name.Should().Be("COVID");
+		vaccine.Id.Should().NotBeEmpty();
+	}
+
+	[Fact]
+	public void Constructor_WithEmptyName_ThrowsException()
+	{
+		Action act = () => new Vaccine("");
+
+		act.Should().Throw<DomainException>()
+			.WithMessage("Name is required.");
+	}
+
+	[Fact]
+	public void UpdateName_WithValidName_Updates()
+	{
+		var vaccine = new Vaccine("Old");
+
+		vaccine.UpdateName("New");
+
+		vaccine.Name.Should().Be("New");
+	}
+
+	[Fact]
+	public void UpdateName_WithEmptyName_ThrowsException()
+	{
+		var vaccine = new Vaccine("Valid");
+
+		Action act = () => vaccine.UpdateName("");
+
+		act.Should().Throw<DomainException>()
+			.WithMessage("Name is required.");
+	}
+}
