363952338e5ee1decb1505786d43308f59755f56
Ghitado <thiaguinho.020405@gmail.com>
2025-11-19 23:57:44 -0300

    feat(infra/di): add DI and IoC file of the infra layer

---
+++ src/VaccinationManager.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VaccinationManager.Domain.Repositories;
+using VaccinationManager.Infrastructure.Persistence;
+using VaccinationManager.Infrastructure.Persistence.Repositories;
+
+namespace VaccinationManager.Infrastructure;
+
+public static class DependencyInjection
+{
+	public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+	{
+		var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+		services.AddDbContext<VaccinationManagerDbContext>(options =>
+			options.UseSqlite(connectionString));
+
+		services.AddScoped<IPersonRepository, PersonRepository>();
+		services.AddScoped<IVaccineRepository, VaccineRepository>();
+		services.AddScoped<IVaccinationRecordRepository, VaccinationRecordRepository>();
+	}
+}
+
