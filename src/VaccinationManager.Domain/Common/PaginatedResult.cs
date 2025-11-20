453a6a9088444688d3a4f964529cdf90ee76ac5c
Ghitado <thiaguinho.020405@gmail.com>
2025-11-19 23:50:51 -0300

    feat(domain/common): add PaginatedResult for pagination throughout the app

---
+++ src/VaccinationManager.Domain/Common/PaginatedResult.cs
@@ -0,0 +1,17 @@
+﻿namespace VaccinationManager.Domain.Common;
+
+public sealed class PaginatedResult<T>
+{
+	public List<T> Items { get; init; } = [];
+	public int TotalCount { get; init; }
+	public int PageNumber { get; init; }
+	public int PageSize { get; init; } 
+
+	public PaginatedResult(List<T> items, int totalCount, int pageNumber, int pageSize)
+	{
+		Items = items ?? new List<T>();
+		TotalCount = totalCount;
+		PageNumber = pageNumber;
+		PageSize = pageSize;
+	}
+}
