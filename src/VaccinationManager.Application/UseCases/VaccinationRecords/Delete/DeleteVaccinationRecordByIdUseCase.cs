1785c1b9f54c09fb131960946a2749d2e6cb168b
Ghitado <thiaguinho.020405@gmail.com>
2025-11-20 00:08:11 -0300

    feat(application/usecases): add delete VaccinationRecord by id use cases

---
+++ src/VaccinationManager.Application/UseCases/VaccinationRecords/Delete/DeleteVaccinationRecordByIdUseCase.cs
@@ -0,0 +1,27 @@
+﻿using VaccinationManager.Domain.Repositories;
+
+namespace VaccinationManager.Application.UseCases.VaccinationRecords.Delete;
+
+public class DeleteVaccinationRecordByIdUseCase : IDeleteVaccinationRecordByIdUseCase
+{
+	private readonly IVaccinationRecordRepository _recordRepository;
+
+	public DeleteVaccinationRecordByIdUseCase(IVaccinationRecordRepository recordRepository)
+	{
+		_recordRepository = recordRepository;
+	}
+
+	public async Task Execute(Guid recordId)
+	{
+		if (recordId == Guid.Empty)
+			throw new ArgumentException("Invalid vaccination record id");
+
+		var record = await _recordRepository.GetById(recordId);
+
+		if (record is null)
+			throw new ArgumentException("Vaccination record not found");
+
+		await _recordRepository.Delete(record);
+	}
+}
+
---
+++ src/VaccinationManager.Application/UseCases/VaccinationRecords/Delete/IDeleteVaccinationRecordByIdUseCase.cs
@@ -0,0 +1,6 @@
+﻿namespace VaccinationManager.Application.UseCases.VaccinationRecords.Delete;
+
+public interface IDeleteVaccinationRecordByIdUseCase
+{
+	Task Execute(Guid recordId);
+}
