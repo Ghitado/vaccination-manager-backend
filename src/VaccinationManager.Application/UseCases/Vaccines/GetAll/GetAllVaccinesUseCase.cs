92d8f5a6c9102403ded2c2f510a44143a71095e8
Ghitado <thiaguinho.020405@gmail.com>
2025-11-20 00:06:01 -0300

    feat(application/usecases): add get all Vaccine use cases

---
+++ src/VaccinationManager.Application/UseCases/Vaccines/GetAll/GetAllVaccinesUseCase.cs
@@ -0,0 +1,30 @@
+﻿using Mapster;
+using VaccinationManager.Application.Dtos.Vaccines;
+using VaccinationManager.Domain.Common;
+using VaccinationManager.Domain.Repositories;
+
+namespace VaccinationManager.Application.UseCases.Vaccines.GetAll;
+
+public class GetAllVaccinesUseCase : IGetAllVaccinesUseCase
+{
+	private readonly IVaccineRepository _vaccineRepository;
+
+	public GetAllVaccinesUseCase(IVaccineRepository vaccineRepository)
+	{
+		_vaccineRepository = vaccineRepository;	
+	}
+
+	public async Task<PaginatedResult<VaccineResponse>> Execute(int? pageNumber, int? pageSize)
+	{
+		if (pageNumber is < 1)
+			throw new ArgumentException("pageNumber must be >= 1", nameof(pageNumber));
+
+		if (pageSize is <= 0)
+			throw new ArgumentException("pageSize must be > 0", nameof(pageSize));
+
+		var vaccines = await _vaccineRepository.GetAll(pageNumber, pageSize);
+
+		return vaccines.Adapt<PaginatedResult<VaccineResponse>>();
+	}
+}
+
---
+++ src/VaccinationManager.Application/UseCases/Vaccines/GetAll/IGetAllVaccinesUseCase.cs
@@ -0,0 +1,10 @@
+﻿using VaccinationManager.Application.Dtos.Persons;
+using VaccinationManager.Application.Dtos.Vaccines;
+using VaccinationManager.Domain.Common;
+
+namespace VaccinationManager.Application.UseCases.Vaccines.GetAll;
+
+public interface IGetAllVaccinesUseCase
+{
+	Task<PaginatedResult<VaccineResponse>> Execute(int? pageNumber, int? pageSize);
+}
