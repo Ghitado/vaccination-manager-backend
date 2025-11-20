081e48b0c7d925b7684ba6394f04036e20ad8def
Ghitado <thiaguinho.020405@gmail.com>
2025-11-19 23:55:40 -0300

    feat(infra/persistence): add dbcontext to connect with sqlite using ef core

---
+++ src/VaccinationManager.Infrastructure/Persistence/VaccinationManagerDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VaccinationManager.Domain.Entities;
+
+namespace VaccinationManager.Infrastructure.Persistence;
+
+public class VaccinationManagerDbContext : DbContext
+{
+	public VaccinationManagerDbContext(DbContextOptions<VaccinationManagerDbContext> options)
+		: base(options) { }
+
+	public DbSet<Person> Persons { get; set; }
+	public DbSet<Vaccine> Vaccines { get; set; }
+	public DbSet<VaccinationRecord> VaccinationRecords { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.ApplyConfigurationsFromAssembly(typeof(VaccinationManagerDbContext).Assembly);
+		base.OnModelCreating(modelBuilder);
+	}
+}
+
