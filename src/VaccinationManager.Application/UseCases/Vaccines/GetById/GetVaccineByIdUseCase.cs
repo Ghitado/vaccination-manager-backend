2fd99955c6d7f00db374a358020db778af5efc5f
Ghitado <thiaguinho.020405@gmail.com>
2025-11-20 00:05:43 -0300

    feat(application/usecases): add get Vaccine by id use cases

---
+++ src/VaccinationManager.Application/UseCases/Vaccines/GetById/GetVaccineByIdUseCase.cs
@@ -0,0 +1,30 @@
+﻿using Mapster;
+using VaccinationManager.Application.Dtos.Vaccines;
+using VaccinationManager.Domain.Entities;
+using VaccinationManager.Domain.Repositories;
+
+namespace VaccinationManager.Application.UseCases.Vaccines.GetById;
+
+public class GetVaccineByIdUseCase : IGetVaccineByIdUseCase
+{
+	private readonly IVaccineRepository _vaccineRepository;
+
+	public GetVaccineByIdUseCase(IVaccineRepository vaccineRepository)
+	{
+		_vaccineRepository = vaccineRepository;
+	}
+
+	public async Task<VaccineResponse?> Execute(Guid id)
+	{
+		if (id == Guid.Empty)
+			throw new ArgumentException("Invalid id");
+
+		var vaccine = await _vaccineRepository.GetById(id);
+
+		if (vaccine is null)
+			throw new ArgumentException("Vaccine not found");
+
+		return vaccine.Adapt<VaccineResponse>();
+	}
+}
+
---
+++ src/VaccinationManager.Application/UseCases/Vaccines/GetById/IGetVaccineByIdUseCase.cs
@@ -0,0 +1,8 @@
+﻿using VaccinationManager.Application.Dtos.Vaccines;
+
+namespace VaccinationManager.Application.UseCases.Vaccines.GetById;
+
+public interface IGetVaccineByIdUseCase
+{
+	Task<VaccineResponse?> Execute(Guid id);
+}
