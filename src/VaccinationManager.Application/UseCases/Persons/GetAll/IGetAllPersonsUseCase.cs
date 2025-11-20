ee6744033a6a0f83c791677b1d85f40830251f1d
Ghitado <thiaguinho.020405@gmail.com>
2025-11-20 00:04:50 -0300

    feat(application/usecases): add get all Person use cases

---
+++ src/VaccinationManager.Application/UseCases/Persons/GetAll/IGetAllPersonsUseCase.cs
@@ -0,0 +1,9 @@
+﻿using VaccinationManager.Application.Dtos.Persons;
+using VaccinationManager.Domain.Common;
+
+namespace VaccinationManager.Application.UseCases.Persons.GetAll;
+
+public interface IGetAllPersonsUseCase
+{
+	Task<PaginatedResult<PersonResponse>> Execute(int? pageNumber, int? pageSize);
+}
