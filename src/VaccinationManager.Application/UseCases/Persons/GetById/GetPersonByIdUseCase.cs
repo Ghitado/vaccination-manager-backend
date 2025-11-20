3ad6c64aefb7f13960a04af07c55dc8fbec3e419
Ghitado <thiaguinho.020405@gmail.com>
2025-11-20 00:04:31 -0300

    feat(application/usecases): add get Person by id use cases

---
+++ src/VaccinationManager.Application/UseCases/Persons/GetById/GetPersonByIdUseCase.cs
@@ -0,0 +1,29 @@
+﻿using Mapster;
+using VaccinationManager.Application.Dtos.Persons;
+using VaccinationManager.Domain.Repositories;
+
+namespace VaccinationManager.Application.UseCases.Persons.GetById;
+
+public class GetPersonByIdUseCase : IGetPersonByIdUseCase
+{
+	private readonly IPersonRepository _repository;
+
+	public GetPersonByIdUseCase(IPersonRepository repository)
+	{
+		_repository = repository;
+	}
+
+	public async Task<PersonResponse> Execute(Guid id)
+	{
+		if (id == Guid.Empty)
+			throw new ArgumentException("Invalid id");
+
+		var person = await _repository.GetById(id);
+
+		if (person is null)
+			throw new ArgumentException("Person not found");
+
+		return person.Adapt<PersonResponse>();
+	}
+}
+
---
+++ src/VaccinationManager.Application/UseCases/Persons/GetById/IGetPersonByIdUseCase.cs
@@ -0,0 +1,8 @@
+﻿using VaccinationManager.Application.Dtos.Persons;
+
+namespace VaccinationManager.Application.UseCases.Persons.GetById;
+
+public interface IGetPersonByIdUseCase
+{
+	Task<PersonResponse> Execute(Guid id);
+}
