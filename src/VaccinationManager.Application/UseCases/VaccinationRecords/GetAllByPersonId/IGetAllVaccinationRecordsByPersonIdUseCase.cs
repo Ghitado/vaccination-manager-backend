8b9958a3a07f8079f58b86bd0f997e94e6b27d40
Ghitado <thiaguinho.020405@gmail.com>
2025-11-20 00:07:59 -0300

    feat(application/usecases): add get all VaccinationRecord by Person's id use cases

---
+++ src/VaccinationManager.Application/UseCases/VaccinationRecords/GetAllByPersonId/IGetAllVaccinationRecordsByPersonIdUseCase.cs
@@ -0,0 +1,9 @@
+﻿using VaccinationManager.Application.Dtos.VaccinationRecords;
+using VaccinationManager.Domain.Common;
+
+namespace VaccinationManager.Application.UseCases.VaccinationRecords.GetByPersonId;
+
+public interface IGetAllVaccinationRecordsByPersonIdUseCase
+{
+	Task<PaginatedResult<VaccinationRecordResponse>> Execute(Guid personId, int? pageNumber, int? pageSize);
+}
