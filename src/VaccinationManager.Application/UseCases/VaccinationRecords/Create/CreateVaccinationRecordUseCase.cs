a4b124a9c7b638179148c3deda24b5b333e0e270
Ghitado <thiaguinho.020405@gmail.com>
2025-11-20 00:06:26 -0300

    feat(application/usecases): add create VaccinationRecord use cases

---
+++ src/VaccinationManager.Application/UseCases/VaccinationRecords/Create/CreateVaccinationRecordUseCase.cs
@@ -0,0 +1,41 @@
+﻿using Mapster;
+using VaccinationManager.Application.Dtos.VaccinationRecords;
+using VaccinationManager.Domain.Entities;
+using VaccinationManager.Domain.Repositories;
+
+namespace VaccinationManager.Application.UseCases.VaccinationRecords.Create;
+
+public class CreateVaccinationRecordUseCase : ICreateVaccinationRecordUseCase
+{
+	private readonly IVaccinationRecordRepository _recordRepository;
+	private readonly IPersonRepository _personRepository;
+	private readonly IVaccineRepository _vaccineRepository;
+
+	public CreateVaccinationRecordUseCase(
+		IVaccinationRecordRepository recordRepository,
+		IPersonRepository personRepository,
+		IVaccineRepository vaccineRepository)
+	{
+		_recordRepository = recordRepository;
+		_personRepository = personRepository;
+		_vaccineRepository = vaccineRepository;
+	}
+
+	public async Task<VaccinationRecordResponse> Execute(CreateVaccinationRecordRequest request)
+	{
+		var person = await _personRepository.GetById(request.PersonId)
+			?? throw new ArgumentException("Person not found");
+
+		var vaccine = await _vaccineRepository.GetById(request.VaccineId)
+			?? throw new ArgumentException("Vaccine not found");
+
+		if (request.Dose is <= 0)
+			throw new ArgumentException("Dose must be greater than 0");
+
+		var record = new VaccinationRecord(person.Id, vaccine.Id, request.Date, request.Dose);
+		await _recordRepository.Add(record);
+
+		return record.Adapt<VaccinationRecordResponse>();
+	}
+}
+
---
+++ src/VaccinationManager.Application/UseCases/VaccinationRecords/Create/ICreateVaccinationRecordUseCase.cs
@@ -0,0 +1,8 @@
+﻿using VaccinationManager.Application.Dtos.VaccinationRecords;
+
+namespace VaccinationManager.Application.UseCases.VaccinationRecords.Create;
+
+public interface ICreateVaccinationRecordUseCase
+{
+	Task<VaccinationRecordResponse> Execute(CreateVaccinationRecordRequest request);
+}
