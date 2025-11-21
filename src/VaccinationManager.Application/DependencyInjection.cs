d35d4dc0cf806d959fe0eec3d769fcd876d129cc
Ghitado <thiaguinho.020405@gmail.com>
2025-11-20 23:05:24 -0300

    chore(application/di): update names and add mapping configuration

--- src/VaccinationManager.Application/DependencyInjection.cs
+++ src/VaccinationManager.Application/DependencyInjection.cs
@@ -1,16 +1,14 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Mapster;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 using VaccinationManager.Application.UseCases.Persons.Create;
 using VaccinationManager.Application.UseCases.Persons.Delete;
-using VaccinationManager.Application.UseCases.Persons.GetAll;
 using VaccinationManager.Application.UseCases.Persons.GetById;
+using VaccinationManager.Application.UseCases.Persons.GetPaginated;
 using VaccinationManager.Application.UseCases.VaccinationRecords.Create;
 using VaccinationManager.Application.UseCases.VaccinationRecords.Delete;
-using VaccinationManager.Application.UseCases.VaccinationRecords.GetAll;
-using VaccinationManager.Application.UseCases.VaccinationRecords.GetById;
-using VaccinationManager.Application.UseCases.VaccinationRecords.GetByPersonId;
 using VaccinationManager.Application.UseCases.Vaccines.Create;
-using VaccinationManager.Application.UseCases.Vaccines.GetAll;
-using VaccinationManager.Application.UseCases.Vaccines.GetById;
+using VaccinationManager.Application.UseCases.Vaccines.GetPaginated;
 
 namespace VaccinationManager.Application;
 
@@ -19,19 +17,17 @@ public static class DependencyInjection
 	public static void AddApplication(this IServiceCollection services)
 	{
 		services.AddScoped<ICreatePersonUseCase, CreatePersonUseCase>();
-		services.AddScoped<IGetAllPersonsUseCase, GetAllPersonsUseCase>();
+		services.AddScoped<IGetPaginatedPersonsUseCase, GetPaginatedPersonsUseCase>();
 		services.AddScoped<IGetPersonByIdUseCase, GetPersonByIdUseCase>();
 		services.AddScoped<IDeletePersonByIdUseCase, DeletePersonByIdUseCase>();
 
 		services.AddScoped<ICreateVaccineUseCase, CreateVaccineUseCase>();
-		services.AddScoped<IGetAllVaccinesUseCase, GetAllVaccinesUseCase>();
-		services.AddScoped<IGetVaccineByIdUseCase, GetVaccineByIdUseCase>();
+		services.AddScoped<IGetPaginatedVaccinesUseCase, GetPaginatedVaccinesUseCase>();
 
 		services.AddScoped<ICreateVaccinationRecordUseCase, CreateVaccinationRecordUseCase>();
-		services.AddScoped<IGetAllVaccinationRecordsUseCase, GetAllVaccinationRecordsUseCase>();
-		services.AddScoped<IGetAllVaccinationRecordsByPersonIdUseCase, GetAllVaccinationRecordsByPersonIdUseCase>();
-		services.AddScoped<IGetVaccinationRecordByIdUseCase, GetVaccinationRecordByIdUseCase>();
 		services.AddScoped<IDeleteVaccinationRecordByIdUseCase, DeleteVaccinationRecordByIdUseCase>();
+
+		TypeAdapterConfig.GlobalSettings.Scan(Assembly.GetExecutingAssembly());
 	}
 }
 
