6daccd3700c45919b4d5c827cc5574211c444096
Ghitado <thiaguinho.020405@gmail.com>
2025-11-20 23:04:34 -0300

    feat(application/mapping): configure mapping from VaccinationRecord class to VaccinationRecordResponse dto list

---
+++ src/VaccinationManager.Application/Mapping/MapsterConfig.cs
@@ -0,0 +1,15 @@
+﻿using Mapster;
+using VaccinationManager.Application.Dtos.VaccinationRecords;
+using VaccinationManager.Domain.Entities;
+
+namespace VaccinationManager.Application.Mapping;
+
+public class MapsterConfig : IRegister
+{
+	public void Register(TypeAdapterConfig config)
+	{
+		config.NewConfig<VaccinationRecord, VaccinationRecordResponse>()
+			.Map(dest => dest.VaccineName, src => src.Vaccine!.Name);
+	}
+}
+
