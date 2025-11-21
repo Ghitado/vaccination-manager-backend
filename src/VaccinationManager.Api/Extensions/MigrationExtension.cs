19fc818d6c3f381110f6971e08cad3fba8dc090e
Ghitado <thiaguinho.020405@gmail.com>
2025-11-20 23:09:21 -0300

    feat(api): add docker support and enable automatic migration in dev

---
+++ src/VaccinationManager.Api/Extensions/MigrationExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VaccinationManager.Infrastructure.Persistence;
+
+namespace VaccinationManager.Api.Extensions;
+
+public static class MigrationExtension
+{
+	public static void ApplyMigrations(this IApplicationBuilder app)
+	{
+		using var scope = app.ApplicationServices.CreateScope();
+		var dbContext = scope.ServiceProvider.GetRequiredService<VaccinationManagerDbContext>();
+
+		var pendingMigrations = dbContext.Database.GetPendingMigrations();
+
+		if (pendingMigrations.Any())
+			dbContext.Database.Migrate();
+	}
+}
+
--- src/VaccinationManager.Api/Program.cs
+++ src/VaccinationManager.Api/Program.cs
@@ -1,6 +1,6 @@
-using Microsoft.OpenApi;
 using Microsoft.OpenApi.Models;
 using System.Reflection;
+using VaccinationManager.Api.Extensions;
 using VaccinationManager.Application;
 using VaccinationManager.Infrastructure;
 
@@ -23,22 +23,6 @@ builder.Services.AddSwaggerGen(c =>
 	var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
 	if (File.Exists(xmlPath))
 		c.IncludeXmlComments(xmlPath);
-
-	c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-	{
-		Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
-		Name = "Authorization",
-		In = ParameterLocation.Header,
-		Type = SecuritySchemeType.ApiKey,
-		Scheme = "Bearer"
-	});
-	c.AddSecurityRequirement(new OpenApiSecurityRequirement
-	{
-		{
-			new OpenApiSecurityScheme { Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" } },
-			Array.Empty<string>()
-		}
-	});
 });
 
 builder.Services.AddInfrastructure(builder.Configuration);
@@ -60,6 +44,7 @@ if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
 	app.UseSwaggerUI();
+	app.ApplyMigrations();
 }
 
 app.UseHttpsRedirection();
