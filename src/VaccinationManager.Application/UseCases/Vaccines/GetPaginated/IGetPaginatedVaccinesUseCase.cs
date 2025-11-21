76f80276b2a0d7212d57284759d2b288994a0835
Ghitado <thiaguinho.020405@gmail.com>
2025-11-20 23:01:38 -0300

    feat(application/usecases): add get Vaccines with PaginatedResult

---
+++ src/VaccinationManager.Application/UseCases/Vaccines/GetPaginated/IGetPaginatedVaccinesUseCase.cs
@@ -0,0 +1,9 @@
+﻿using VaccinationManager.Application.Dtos.Vaccines;
+using VaccinationManager.Domain.Common;
+
+namespace VaccinationManager.Application.UseCases.Vaccines.GetPaginated;
+
+public interface IGetPaginatedVaccinesUseCase
+{
+	Task<PaginatedResult<VaccineResponse>> Execute(int pageNumber, int pageSize);
+}
