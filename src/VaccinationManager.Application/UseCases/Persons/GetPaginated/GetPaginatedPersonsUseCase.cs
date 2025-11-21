e06a6cfd2ad9f8e9ff39a3d299f233f5dcd05153
Ghitado <thiaguinho.020405@gmail.com>
2025-11-20 23:01:52 -0300

    feat(application/usecases): add get Persons with PaginatedResult

---
+++ src/VaccinationManager.Application/UseCases/Persons/GetPaginated/GetPaginatedPersonsUseCase.cs
@@ -0,0 +1,30 @@
+﻿using Mapster;
+using VaccinationManager.Application.Dtos.Persons;
+using VaccinationManager.Domain.Common;
+using VaccinationManager.Domain.Repositories;
+
+namespace VaccinationManager.Application.UseCases.Persons.GetPaginated;
+
+public class GetPaginatedPersonsUseCase : IGetPaginatedPersonsUseCase
+{
+	private readonly IPersonRepository _repository;
+
+	public GetPaginatedPersonsUseCase(IPersonRepository repository)
+	{
+		_repository = repository;
+	}
+
+	public async Task<PaginatedResult<PaginatedPersonResponse>> Execute(int pageNumber, int pageSize)
+	{
+		if (pageNumber is < 1)
+			throw new ArgumentException("pageNumber must be >= 1", nameof(pageNumber));
+
+		if (pageSize is <= 0)
+			throw new ArgumentException("pageSize must be > 0", nameof(pageSize));
+
+		var persons = await _repository.GetPaginated(pageNumber, pageSize);
+
+		return persons.Adapt<PaginatedResult<PaginatedPersonResponse>>();
+	}
+}
+
---
+++ src/VaccinationManager.Application/UseCases/Persons/GetPaginated/IGetPaginatedPersonsUseCase.cs
@@ -0,0 +1,9 @@
+﻿using VaccinationManager.Application.Dtos.Persons;
+using VaccinationManager.Domain.Common;
+
+namespace VaccinationManager.Application.UseCases.Persons.GetPaginated;
+
+public interface IGetPaginatedPersonsUseCase
+{
+	Task<PaginatedResult<PaginatedPersonResponse>> Execute(int pageNumber, int pageSize);
+}
