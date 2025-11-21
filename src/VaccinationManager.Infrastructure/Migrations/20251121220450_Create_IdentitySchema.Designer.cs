41079671a25c580923f560c3b06fe8dc7a9e1557
Ghitado <thiaguinho.020405@gmail.com>
2025-11-21 20:05:39 -0300

    chore(infra/migrations): add identity tables migrations

--- src/VaccinationManager.Infrastructure/Migrations/20251121220450_Create_IdentitySchema.Designer.cs
+++ src/VaccinationManager.Infrastructure/Migrations/20251121220450_Create_IdentitySchema.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VaccinationManager.Infrastructure.Persistence;
 
@@ -10,9 +11,11 @@ using VaccinationManager.Infrastructure.Persistence;
 namespace VaccinationManager.Infrastructure.Migrations
 {
     [DbContext(typeof(VaccinationManagerDbContext))]
-    partial class VaccinationManagerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251121220450_Create_IdentitySchema")]
+    partial class Create_IdentitySchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.21");
